0f222de7668de579282dd6d2a57b3202a48b5657
hiarief <arhistr14@gmail.com>
2023-01-04 10:47:12 +0700

    */sidebar

--- resources/views/layouts/siode/sidebar.blade.php
+++ resources/views/layouts/siode/sidebar.blade.php
@@ -1,26 +1,22 @@
 <aside class="main-sidebar sidebar-dark-warning elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link text-sm">
-        <img src="{{ URL::asset('assets/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
-            class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+    <a href="{{ route('siode.dashboard.index') }}" class="brand-link text-sm">
+        <img src="{{ URL::asset('images/siode.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+            style="opacity: .8">
+        <span class="brand-text font-weight-light">SiODe</span>
     </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
         <!-- Sidebar user panel (optional) -->
-        {{--  <div class="user-panel d-flex mt-2 mb-2 pb-2">
+        <div class="user-panel d-flex mt-2 mb-2 pb-2">
             <div class="image">
-                <img src="{{ URL::asset('assets/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2"
-                    alt="User Image">
+                <img src="{{ URL::asset('images/desa.png') }}" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">{!! Auth::user()->name !!}</a>
-                <small class="text-warning">
-                    Sebagai : {{ Auth::user()->roles->pluck('name')[0] ?? '' }}
-                </small>
+                <a href="{{ route('siode.dashboard.index') }}" class="d-block">Desa Cisoka</a>
             </div>
-        </div>  --}}
+        </div>
 
         <div class="form-inline d-flex mt-2 mb-2 pb-2">
             <div class="input-group" data-widget="sidebar-search">
@@ -35,18 +31,227 @@
         </div>
 
 
-
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column nav-compact nav-flat nav-child-indent nav-collapse-hide-child text-sm"
                 data-widget="treeview" role="menu" data-accordion="false">
+                <li class="nav-header">MENU UTAMA</li>
                 <li class="nav-item">
                     <a href="{{ route('siode.dashboard.index') }}" class="nav-link {!! request()->is(['siode/dashboard']) || request()->is(['siode/dashboard/*']) ? 'active' : '' !!}">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <i class="nav-icon fas fa-solid fa-house-chimney"></i>
                         <p>
                             Dashboard
                         </p>
                     </a>
                 </li>
+
+                {{--  Info Desa  --}}
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-solid fa-circle-info text-danger"></i>
+                        <p>
+                            Info Desa
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview" style="display: none;">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="far fa-circle nav-icon text-red"></i>
+                                <p>Identitas Desa</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Wilayah Administratif</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="far fa-circle nav-icon text-yellow"></i>
+                                <p>Pemerintahan Desa</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="far fa-circle nav-icon text-green"></i>
+                                <p>Status Desa</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="far fa-circle nav-icon text-blue"></i>
+                                <p>Lembaga Desa</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
+                {{--  Kependudukan  --}}
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-solid fa-people-group text-info"></i>
+                        <p>
+                            Kependudukan
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview" style="display: none;">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="far fa-circle nav-icon text-blue"></i>
+                                <p>Penduduk</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="far fa-circle nav-icon text-green"></i>
+                                <p>Keluarga</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="far fa-circle nav-icon text-yellow"></i>
+                                <p>Rumah Tangga</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="far fa-circle nav-icon text-red"></i>
+                                <p>Kelompok</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="far fa-circle nav-icon text-purple"></i>
+                                <p>Data Suplemen</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="far fa-circle nav-icon text-white"></i>
+                                <p>Calon Pemilih</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
+                {{--  Buku Administrasi Desa  --}}
+
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-book text-success"></i>
+                        <p>
+                            Buku Administrasi Desa
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="far fa-circle nav-icon text-blue"></i>
+                                <p>Umum</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="far fa-circle nav-icon text-white"></i>
+                                <p>Penduduk</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="far fa-circle nav-icon text-green"></i>
+                                <p>Pembangunan</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="far fa-circle nav-icon text-red"></i>
+                                <p>Arsip Desa</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
+                {{--  layanan Surat  --}}
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-edit text-primary"></i>
+                        <p>
+                            Layanan Surat
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="far fa-circle nav-icon text-green"></i>
+                                <p>Pengaturan Surat</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="far fa-circle nav-icon text-yellow"></i>
+                                <p>Cetak Surat</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="far fa-circle nav-icon text-purple"></i>
+                                <p>Arsip Layanan</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="far fa-circle nav-icon text-red"></i>
+                                <p>Panduan</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="far fa-circle nav-icon text-blue"></i>
+                                <p>Daftar Persyaratan</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
+                {{--  Statistik  --}}
+
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-chart-pie text-warning"></i>
+                        <p>
+                            Statistik
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="far fa-circle nav-icon text-blue"></i>
+                                <p>Statistik Kependudukan</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="far fa-circle nav-icon text-white"></i>
+                                <p>Laporan Bulanan</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="far fa-circle nav-icon text-purple"></i>
+                                <p>Laporan Penduduk</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
+
+                {{--  user  --}}
+                <li class="nav-header">USER MANAGE</li>
                 <li class="nav-item {!! request()->is(['permissions', 'roles', 'users']) || request()->is(['permissions/*', 'roles/*', 'users/*'])
                     ? 'menu-open'
                     : '' !!}">
